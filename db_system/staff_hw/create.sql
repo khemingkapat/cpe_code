263cb51b036a2c5421a0837a0489add34805a46b
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-01-23 13:36:18 +0700

    redefine staff_id to serial and set start with 101

--- db_system/staff_hw/create.sql
+++ db_system/staff_hw/create.sql
@@ -1,8 +1,11 @@
 CREATE TABLE staff (
-    staff_id varchar(4) PRIMARY KEY,
+    staff_id serial PRIMARY KEY,
     firstName varchar(50) NOT NULL,
     lastName varchar(50) NOT NULL,
     DOB varchar(11) NOT NULL,
     position varchar(50) NOT NULL,
     salary varchar(7) NOT NULL
 );
+
+ALTER SEQUENCE staff_staff_id_seq
+    RESTART WITH 101;
