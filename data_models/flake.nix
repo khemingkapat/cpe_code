246af91ec6371b1af2efe8e725a503e7a029a082
Lin Xianyi <iynaix@gmail.com>
2025-01-31 16:45:25 +0800

    Add jupyterlab-vim package

--- data_models/flake.nix
+++ data_models/flake.nix
@@ -6,48 +6,29 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         python = pkgs.python311;
 
-
         # 🔹 Define Python environment including jupyterlab_vim
-        pythonEnv = python.withPackages (ps: with ps;
-          [
+        pythonEnv = python.withPackages (
+          ps: with ps; [
             numpy
             pandas
             matplotlib
             nbconvert
             jupyterlab
-            (ps.buildPythonPackage rec {
-              pname = "jupyterlab-vim";
-              version = "4.1.4";
-              pyproject = true;
-
-              src = fetchPypi {
-                pname = "jupyterlab_vim";
-                inherit version;
-                hash = "sha256-q/KJGq+zLwy5StmDIa5+vL4Mq+Uj042A1WnApQuFIlo=";
-              };
-
-              build-system = with pkgs; [
-                hatch-nodejs-version
-                hatchling
-                jupyterlab
-              ];
-
-              dependencies = with pkgs; [
-                hatch-jupyter-builder
-                jupyterlab
-              ];
-
-              pythonImportsCheck = [
-                "jupyterlab_vim"
-              ];
-            })
-          ]);
+            self.packages.${system}.jupyterlab-vim
+          ]
+        );
 
       in
       {
@@ -57,12 +38,20 @@
             pandoc
             texlive.combined.scheme-full
           ];
-          buildInputs = [ pythonEnv pkgs.nodejs ];
+          buildInputs = [
+            pythonEnv
+            pkgs.nodejs
+          ];
 
           shellHook = ''
             	  echo "✅ Setting up JupyterLab with Vim mode...
                   echo "✅ JupyterLab Vim is ready!"
           '';
         };
-      });
+
+        packages = {
+          jupyterlab-vim = pkgs.python311Packages.callPackage ./jupyterlab-vim.nix { };
+        };
+      }
+    );
 }
