cbf0fbe8728d5062fb57b790310b44dc396307e3
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-01-22 23:01:15 +0700

    fix printing result

--- ai/search/ids.jl
+++ ai/search/ids.jl
@@ -2,7 +2,7 @@ include("dls.jl")
 function ids(graph, mapping, start_node, end_node,depth_limit=100)
     depth = 0
     while true
-        println("Searching with depth limit: ", depth)
+        print("Searching with depth limit: ", depth,"=>")
         # Call your existing dls function
 	result_found,path = dls(graph, mapping, start_node, end_node, depth)
 
