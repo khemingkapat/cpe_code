c49b326895f617ab2fbb35381f1ba9bbd6c958a5
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-01-22 22:57:13 +0700

    use all algo and print the result

--- ai/search/main.jl
+++ ai/search/main.jl
@@ -2,8 +2,27 @@ using Graphs, SimpleWeightedGraphs, GraphRecipes,Plots
 include("create_graph.jl")
 include("dls.jl")
 include("ids.jl")
+include("uniform_cost.jl")
+include("bfs.jl")
 
 graph, mapping = create_graph()
 
-result = ids(graph, mapping, "A", "B")
-println(result)
+result,path = bfs(graph, mapping, "A", "B")
+println("bfs","|",result,"|",path)
+println()
+
+result,path = uniform_cost(graph, mapping, "A", "B")
+println("uniform_cost","|",result,"|",path)
+println()
+
+result,path = dls(graph, mapping, "A", "B")
+println("dfs","|",result,"|",path)
+println()
+
+result,path = dls(graph, mapping, "A", "B",3)
+println("dls(3)","|",result,"|",path)
+println()
+
+result,path = ids(graph, mapping, "A", "B")
+println("ids","|",result,"|",path)
+println()
