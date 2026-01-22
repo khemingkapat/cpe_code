19b5b2e07776fa1357e02ae83c8b404108fd87d6
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-01-22 22:56:50 +0700

    add backtracking of optimal path

--- ai/search/dls.jl
+++ ai/search/dls.jl
@@ -1,4 +1,4 @@
-function dls(graph, mapping, start_node, end_node,limit=-1)
+function dls(graph, mapping, start_node, end_node, limit=-1)
     start_idx = mapping[start_node]
     end_idx = mapping[end_node]
     reverse_mapping = Dict(v => k for (k, v) in mapping)
@@ -6,35 +6,48 @@ function dls(graph, mapping, start_node, end_node,limit=-1)
     fringe = [start_idx]
     depth_stack = [0]
     visited = Dict(idx => false for idx in values(mapping))
-    current_depth = 1
+    
+    parent = Dict{Int,Int}() 
 
     while !isempty(fringe)
         current_node = popfirst!(fringe)
-	node_depth = popfirst!(depth_stack)
+        node_depth = popfirst!(depth_stack)
         
         if visited[current_node]
             continue
         end
 
-        println(reverse_mapping[current_node])
-        
+	print(reverse_mapping[current_node],",")
+
         if current_node == end_idx
-            return true
+            path = []
+            curr = end_idx
+            while curr != start_idx
+                pushfirst!(path, reverse_mapping[curr])
+                curr = parent[curr]
+            end
+            pushfirst!(path, reverse_mapping[start_idx])
+	    println()
+            return true,path
         end
 
         visited[current_node] = true
        
-	if (limit > 0) && (node_depth > limit)
-	    continue
-	end
+        if (limit >= 0) && (node_depth >= limit)
+            continue
+        end
 
-        children = neighbors(graph, current_node)
-	current_depth += 1
+       children = neighbors(graph, current_node)
         unvisited_children = filter(child -> !visited[child], children)
 
+	for child in unvisited_children
+	    if !haskey(parent, child)
+		parent[child] = current_node
+	    end
+	end
         prepend!(fringe, unvisited_children)
-	prepend!(depth_stack,fill(current_depth,length(unvisited_children)))
+        prepend!(depth_stack, fill(node_depth + 1, length(unvisited_children)))
     end
-    
-    return false
+    println()
+    return false,[] # Return nothing if no path is found
 end
