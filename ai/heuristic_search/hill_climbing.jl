a1018772089d7ed585f549cee62e3f66bf9acd35
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-02-03 19:38:51 +0700

    fix hill climbing to be stateless and remove backtracking

--- ai/heuristic_search/hill_climbing.jl
+++ ai/heuristic_search/hill_climbing.jl
@@ -1,58 +1,46 @@
 function hill_climbing(h_map, start, goal)
     rows, cols = size(h_map)
+    current = start
+    path = [current]
     
-    fringe = [start]
-    
-    visited = Set{Tuple{Int,Int}}()
-    parent = Dict{Tuple{Int,Int}, Tuple{Int,Int}}()
-    
-    while !isempty(fringe)
-        current = pop!(fringe)
+    while current != goal
+        current_h = h_map[current...]
         
-        if current in visited
-            continue
-        end
-        
-        println(current)
-        push!(visited, current)
-
-        if current == goal
-            path = []
-            curr = goal
-            while curr != start
-                pushfirst!(path, curr)
-                curr = parent[curr]
-            end
-            pushfirst!(path, start)
-            return true, path
-        end
-
-        raw_dirs = [
-            (1, 0),  # Down
+        dirs = [
+            (-1, 0), # Up
+            (0, -1), # Left
             (0, 1),  # Right
-            ((0, -1)), # Left
-            (-1, 0)  # Up
+            (1, 0)   # Down
         ]
-
-        neighbors_to_add = []
-        for (di, dj) in raw_dirs
+        
+        best_neighbor = nothing
+        best_h = current_h
+        
+        for (di, dj) in dirs
             neighbor = (current[1] + di, current[2] + dj)
-            if neighbor[1] in 1:rows && neighbor[2] in 1:cols
-                if h_map[neighbor...] != Inf && !(neighbor in visited)
-                    push!(neighbors_to_add, neighbor)
+            
+            if neighbor[1] in 1:rows && neighbor[2] in 1:cols && h_map[neighbor...] != Inf
+                neighbor_h = h_map[neighbor...]
+                
+                if neighbor_h < best_h
+                    best_h = neighbor_h
+                    best_neighbor = neighbor
                 end
             end
         end
-
-        sort!(neighbors_to_add, by = n -> h_map[n...], rev = true)
-
-        for neighbor in neighbors_to_add
-            if !haskey(parent, neighbor)
-                parent[neighbor] = current
-            end
-            push!(fringe, neighbor)
+        
+        if best_neighbor === nothing
+            break
+        else
+            current = best_neighbor
+            push!(path, current)
+            println(current)
         end
     end
-
-    return false, []
+    
+    if current == goal
+	return true, path
+    else
+	return false,nothing
+    end
 end
