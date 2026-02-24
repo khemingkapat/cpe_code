3f24c8f12dc3990f72a1c077a47a6e08500a57e1
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-02-24 21:09:08 +0700

    calling function

---
+++ ai/game_playing/main.jl
@@ -0,0 +1,12 @@
+using Plots, GraphRecipes
+
+include("nim.jl")
+include("minimax.jl")
+include("visualize.jl")
+include("alpha_beta.jl")
+g, node_map, id_to_state = create_nim(2, 1, 1)
+node_values, new_id_to_state = minimax(g, node_map, id_to_state)
+alpha_beta_pruning(g, new_id_to_state)
+
+
+
