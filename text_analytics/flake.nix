6e0c22c9773764ba9a8423393f1915de8502f3b6
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2026-08-13 11:24:11 +0700

    add flake for text analytics course

---
+++ text_analytics/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Text Analytics Course Works";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          uv
+          python311
+          # System libraries needed by pre-compiled Python wheels
+          stdenv.cc.cc.lib
+          zlib
+        ];
+
+        env = {
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib
+            pkgs.zlib
+          ];
+        };
+
+        shellHook = ''
+          echo "Text Analytics Course Works"
+          echo "Run 'uv sync' to install dependencies"
+          echo "Run 'uv run jupyter lab' to start"
+          echo ""
+
+          export SHELL=/home/khemi/.nix-profile/bin/zsh
+          exec /home/khemi/.nix-profile/bin/zsh
+        '';
+      };
+    };
+}
